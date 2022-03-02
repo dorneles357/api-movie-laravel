0fa5bd5d2afc8b48cb22d372e1778935664fc2ee
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-03-02 00:09:41 -0300

    ✅ Configuração para jwt-auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -21,5 +23,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
     ];
+
+    /*JWT - Configurações para autenticação*/
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
 }
